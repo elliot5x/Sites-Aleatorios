f5cde0d42c82712bd55473a9cbd1cdaa462a0919
d3dsec <thefranksinatra67@protonmail.com>
2024-03-16 14:27:13 -0300

    commit

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-## Meu site do imagine dragons hehehe
+## Site aleatorio
 
-- Feito apenas para compreender melhor os fundamentos de html, css e javascript.
-- Talvez eu altere ele no futuro com novas implementações.
-- Qualquer idéia nova que eu tiver, atualizarei o site :3
-
-# atualizações
-
-- Mudei o meu nome no topo da página para um link
-- Mudei o javascript de lugar, agr ele é um arquivo separado com um codigo novo
+- Literalmente um site aleatorio
+- Sempre que der vontade eu volto nesse codigo pra testar coisas novas usando o HTML, CSS e JavaScript
+- Talvez eu adicione novas linguagens mais pra frente, pq o objetivo é justamente treinar programação
--- scripts.js
+++ scripts.js
@@ -1,25 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Espera o DOM ser carregado antes de manipular os elementos
-
-    // Obtém o elemento com o ID 'role'
-    var role = document.getElementById('role');
-
-    // Verifica se o elemento 'role' foi encontrado no DOM
-    if (role) {
-        var ultimoScroll = window.pageYOffset;
-
-        window.onscroll = function () {
-            var novoScroll = window.pageYOffset;
-            if (ultimoScroll > novoScroll) {
-                // Rolar para cima
-                role.style.opacity = "0";
-            } else {
-                // Rolar para baixo
-                role.style.opacity = "0";
-            }
-            ultimoScroll = novoScroll;
-        }
-    } else {
-        console.error("Elemento com ID 'role' não encontrado no DOM.");
-    }
+window.addEventListener('DOMContentLoaded', function () {
+    var meuAudio = document.getElementById('meuAudio');
+    meuAudio.addEventListener('loadedmetadata', function () {
+        meuAudio.currentTime = 5; // 5 segundos
+        meuAudio.volume = 0.3;
+    });
 });
